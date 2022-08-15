081b8fc39cb41d2dcf6de705b74bf197d68aeb3f
robert-rkkr <robert.kucharski.rkkr@gmail.com>
2022-08-15 17:34:45 +0200

    Message object collector

--- autocli/messages/all/collect.py
+++ autocli/messages/all/collect.py
@@ -5,9 +5,22 @@ from django.apps import apps
 #   app name, model name and object ID.
 def collect_object(app_name: str, model_name: str, object_id: int):
 
-    collected_model = apps.get_model(
-        app_label=app_name,
-        model_name=model_name,
-    )
-    collected_object = collected_model.objects.get(pk=object_id)
-    return collected_object
+    # try to collect model based on app and model name:
+    try:
+        collected_model = apps.get_model(
+            app_label=app_name,
+            model_name=model_name,
+        )
+    except:
+        # In case of model collection problem, return false:
+        return False
+    else:
+        # Try to collect object based on object ID:
+        try:
+            collected_object = collected_model.objects.get(pk=object_id)
+        except:
+            # In case of object collection problem, return false:
+            return False
+        else:
+            # Return collected object:
+            return collected_object
